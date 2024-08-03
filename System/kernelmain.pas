4f2eea1daabd624b0806869402463dcca2180c90
TYDQSoft <166505188+TYDQSoft@users.noreply.github.com>
2024-08-03 22:35:35 +0800

    Add files via upload

--- System/kernelmain.pas
+++ System/kernelmain.pas
@@ -3,17 +3,15 @@ program kernelmain;
 uses bootconfig,graphics;
 
 function kernel_main(param:sys_parameter):Pointer;[public,alias:'_start'];
-var Pparam1:Pscreen_config;
-    screenconfig:screen_config;
+var screenconfig:screen_config;
     ptr:Pgraphics_item;
     res:Pqword;
 begin
- Pparam1:=Pointer(param.param_content);
- screenconfig:=Pparam1^;
+ screenconfig:=Pscreen_config(param.param_content)^;
  graphics_heap_initialize;
- ptr:=graphics_heap_getmem(1,1,screenconfig.screen_scanline,screenconfig.screen_height);
- graphics_draw_block(ptr,1,1,screenconfig.screen_scanline,screenconfig.screen_height,$FF,$FF,$FF,$FF);
- graphics_heap_output_to_screen(screenconfig.screen_address,screenconfig.screen_scanline,screenconfig.screen_height);
+ ptr:=graphics_heap_getmem(1,1,screenconfig.screen_width,screenconfig.screen_height);
+ graphics_draw_block(ptr,1,1,screenconfig.screen_width,screenconfig.screen_height,$FF,$FF,$FF,$FF);
+ graphics_heap_output_to_screen(screenconfig.screen_address,screenconfig.screen_width,screenconfig.screen_height);
  res:=allocmem(sizeof(qword));
  res^:=120;
  kernel_main:=res;
