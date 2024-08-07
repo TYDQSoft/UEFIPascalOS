df4c52052950b8707dc6aa464e52ae85d75aa653
TYDQSoft <166505188+TYDQSoft@users.noreply.github.com>
2024-08-07 21:46:26 +0800

    Add files via upload

--- buildaarch64.sh
+++ buildaarch64.sh
@@ -7,14 +7,14 @@
 	/home/tydq/source/compiler/ppca64 -n -O4 -Si -Sc -Sg -Xd -Ur -CX -XXs -Twin64 -Cg BaseUnits/sysinit.pas
 	/home/tydq/source/compiler/ppca64 -n -O4 -Si -Sc -Sg -Xd -Ur -CX -XXs -Xi -Twin64 -Cg -FuBaseUnits -FEBinaries/Installer Installer/uefiinstaller.pas
 	/home/tydq/source/compiler/ppca64 -n -O4 -Si -Sc -Sg -Xd -Ur -CX -XXs -Xi -Twin64 -Cg -FuBaseUnits -FEBinaries/BootLoader BootLoader/uefiloader.pas
-	objcopy -I pei-x86-64 -O efi-app-x86_64 Binaries/Installer/uefiinstaller.dll Binaries/Installer/bootaa64.efi
-	objcopy -I pei-x86-64 -O efi-app-x86_64 Binaries/BootLoader/uefiloader.dll Binaries/BootLoader/bootaa64.efi
+	objcopy -I pei-aarch64-little -O efi-app-aarch64 Binaries/Installer/uefiinstaller.exe Binaries/Installer/bootaa64.efi
+	objcopy -I pei-aarch64-little -O efi-app-aarch64 Binaries/BootLoader/uefiloader.exe Binaries/BootLoader/bootaa64.efi
 	rm -rf BaseUnits/*.ppu BaseUnits/*.o Installer/*.o Installer/*.ppu BootLoader/*.o BootLoader/*.ppu
 	/home/tydq/source/compiler/ppca64 -n -O4 -Si -Sc -Sg -Xd -Ur -Us -CX -Xs -Cg BaseUnits/system.pas
 	/home/tydq/source/compiler/ppca64 -n -O4 -Si -Sc -Sg -Xd -Ur -CX -Xs -Cg BaseUnits/fpintres.pas
 	/home/tydq/source/compiler/ppca64 -n -O4 -Si -Sc -Sg -Xd -Ur -CX -Xs -Cg BaseUnits/sysinit.pas
 	/home/tydq/source/compiler/ppca64 -n -O4 -Si -Sc -Sg -Xd -Ur -CX -Xs -Cg BaseUnits/si_prc.pas
-	/home/tydq/source/compiler/ppca64 -n -O4 -Si -Sc -Sg -Xd -Ur -CX -Xs -Xi -Cg -FuBaseUnits -FEBinaries/System System/kernelmain.pas
+	/home/tydq/source/compiler/ppca64 -n -O4 -Si -Sc -Sg -Xd -Ur -CX -Xs -Xi -Cg -k-pie -FuBaseUnits -FEBinaries/System System/kernelmain.pas
 	objcopy Binaries/System/kernelmain Binaries/System/kernelmain.elf
 	rm -rf BaseUnits/*.ppu BaseUnits/*.o System/*.o System/*.ppu 
 	dd if=/dev/zero of=Binaries/fat.img bs=512 count=131072
