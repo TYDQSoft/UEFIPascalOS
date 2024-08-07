81f314cbf9fe79e61657717848d5c683cfe4a3d7
TYDQSoft <166505188+TYDQSoft@users.noreply.github.com>
2024-08-07 21:44:34 +0800

    Add files via upload

--- System/kernelmain.pas
+++ System/kernelmain.pas
@@ -1,5 +1,7 @@
 program kernelmain;
 
+{$MODE FPC}
+
 uses bootconfig,graphics;
 
 function kernel_main(param:sys_parameter):Pointer;[public,alias:'_start'];
@@ -7,19 +9,19 @@ var screenconfig:screen_config;
     ptr:Pgraphics_item;
     res:Preturn_config;
 begin
- sysheap_initialize;
+ {$ifdef cpux86_64}
+ screenconfig:=Pscreen_config(Psys_parameter($BFEC1900)^.param_content)^;
+ {$else cpux86_64}
  screenconfig:=Pscreen_config(param.param_content)^;
+ {$endif}
  graphics_heap_initialize;
- //ptr:=graphics_heap_getmem(1,1,2560,1600);
- //graphics_draw_block(ptr,1,1,2560,1600,$FF,$FF,$FF,$FF);
- //graphics_heap_output_to_screen(3288334336,2560,1600);
  ptr:=graphics_heap_getmem(1,1,screenconfig.screen_width,screenconfig.screen_height);
  graphics_draw_block(ptr,1,1,screenconfig.screen_width,screenconfig.screen_height,$FF,$FF,$FF,$FF);
  graphics_heap_output_to_screen(screenconfig.screen_address,screenconfig.screen_width,screenconfig.screen_height);
  res:=allocmem(sizeof(return_config));
- res^.content:=Natuint(param.param_content);
- res^.size:=param.param_size^;
- res^.count:=param.param_count;
+ res^.content:=screenconfig.screen_address;
+ res^.size:=screenconfig.screen_width;
+ res^.count:=screenconfig.screen_height;
  kernel_main:=res;
 end;
 
