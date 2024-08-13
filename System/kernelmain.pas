c5ded0aab0d3c3a885ebaa55a33dcd7d5a98557e
TYDQSoft <166505188+TYDQSoft@users.noreply.github.com>
2024-08-13 18:33:23 +0800

    Add files via upload

--- System/kernelmain.pas
+++ System/kernelmain.pas
@@ -4,24 +4,28 @@ program kernelmain;
 
 uses bootconfig,graphics;
 
-function kernel_main(param:sys_parameter):Pointer;[public,alias:'_start'];
-var screenconfig:screen_config;
+function kernel_main(param:Psys_parameter):Pointer;[public,alias:'_start'];
+var readoffset:natuint;
     ptr:Pgraphics_item;
     res:Preturn_config;
+    i:natuint;
 begin
- {$ifdef cpux86_64}
- screenconfig:=Pscreen_config(Psys_parameter($BFEC1900)^.param_content)^;
- {$else cpux86_64}
- screenconfig:=Pscreen_config(param.param_content)^;
- {$endif}
- graphics_heap_initialize;
- ptr:=graphics_heap_getmem(1,1,screenconfig.screen_width,screenconfig.screen_height);
- graphics_draw_block(ptr,1,1,screenconfig.screen_width,screenconfig.screen_height,$FF,$FF,$FF,$FF);
- graphics_heap_output_to_screen(screenconfig.screen_address,screenconfig.screen_width,screenconfig.screen_height);
+ readoffset:=0;
+ compheap:=Pheap_record(param^.param_content+readoffset)^;
+ readoffset:=readoffset+sizeof(heap_record);
+ graphheap:=Pgraphics_heap(param^.param_content+readoffset)^;
+ readoffset:=readoffset+sizeof(graphics_heap);
+ sysheap:=Pheap_record(param^.param_content+readoffset)^;
+ readoffset:=readoffset+sizeof(heap_record);
+ ptr:=graphics_getmem(1,1,graphheap.screenwidth,graphheap.screenheight);
+ graphics_draw_block(ptr,1,1,graphheap.screenwidth,graphheap.screenheight,graphics_white);
+ graphics_output_to_screen;
  res:=allocmem(sizeof(return_config));
- res^.content:=screenconfig.screen_address;
- res^.size:=screenconfig.screen_width;
- res^.count:=screenconfig.screen_height;
+ res^.m1:=graphheap.contentused;
+ res^.m2:=graphheap.contentmax;
+ res^.m3:=Natuint(ptr);
+ res^.m4:=graphheap.segmentcount;
+ res^.m5:=graphheap.segmentcountmax;
  kernel_main:=res;
 end;
 
