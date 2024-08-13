a67e1f0730c963e125874a394fd3f1eaee597bcc
TYDQSoft <166505188+TYDQSoft@users.noreply.github.com>
2024-08-13 18:33:53 +0800

    Add files via upload

--- BootLoader/uefiloader.pas
+++ BootLoader/uefiloader.pas
@@ -2,19 +2,17 @@ program uefiloader;
 
 {$MODE FPC}
 
-uses uefi,tydqfs,binarybase,bootconfig;
+uses uefi,binarybase,bootconfig,graphics;
 
-var proccontent:array[1..67108864] of byte;
+var proccontent:Pointer;
     procsize:dword;
     
-function efi_main(ImageHandle:efi_handle;systemtable:Pefi_system_table):efi_status;[public,alias:'_mainCRTStartup'];
+function efi_main(ImageHandle:efi_handle;systemtable:Pefi_system_table):efi_status;{$ifdef cpux86_64}MS_ABI_Default;{$endif}{$ifdef cpui386}cdecl;{$endif cpui386}[public,alias:'_mainCRTStartup'];
 var {For checking elf file}
-    fsi:tydqfs_system_info;
-    edl:efi_disk_list;
     sfsp:Pefi_simple_file_system_protocol;
     fp:Pefi_file_protocol;
     efsl:efi_file_system_list;
-    i,count:natuint;
+    i,j,count,procindex:natuint;
     status:efi_status;
     finfo:efi_file_info;
     finfosize:natuint;
@@ -40,63 +38,76 @@ var {For checking elf file}
     func:sys_parameter_function;
     funcandparam:sys_parameter_function_and_parameter;
     res:Pointer;
+    {For memory initializtion}
+    memorymap:efi_memory_map;
+    memoryavailable:efi_memory_result;
+    addressoffset:natuint;
+    allocaddress:natuint;
+    {For loaded uefi image}
+    myprotocol:Pefi_loaded_image_protocol;
+    devicehandlesize:natuint;
+    devicehandle:efi_handle;
 begin  
  {Initialize the uefi loader}
- compheap_initialize; sysheap_initialize;
- efi_console_initialize(systemtable,efi_bck_black,efi_lightgrey,500);
- efi_console_enable_mouse_blink(systemtable,false,0);
- edl:=efi_disk_tydq_get_fs_list(systemtable);
- fsi:=tydq_fs_systeminfo_read(edl);
- freemem(edl.disk_block_content); freemem(edl.disk_content); edl.disk_count:=0;
- efsl:=efi_list_all_file_system(systemtable,0);
- i:=1; count:=efsl.file_system_count;
- gpl:=efi_graphics_initialize(systemtable);
+ efi_initialize(ImageHandle,systemtable);
+ efi_console_initialize(efi_bck_black,efi_lightgrey,500);
+ efsl:=efi_list_all_file_system(1);
+ i:=0; count:=efsl.file_system_count;
+ gpl:=efi_graphics_initialize;
  efi_graphics_get_maxwidth_maxheight_and_maxdepth(gpl,1);
  {Detect the kernel file}
  while(i<=count) do
   begin
+   inc(i);
+   if(i>count) then break;
    sfsp:=(efsl.file_system_content+i-1)^;
    sfsp^.OpenVolume(sfsp,fp);
-   fp^.Open(fp,fp,'\EFI\SYSTEM\kernelmain.elf',efi_file_mode_read,efi_file_system);
+   status:=fp^.Open(fp,fp,'\EFI\SYSTEM\kernelmain.elf',efi_file_mode_read,efi_file_system);
+   if(status<>efi_success) then continue;
    fp^.SetPosition(fp,0);
    finfosize:=sizeof(efi_file_info);
    fp^.GetInfo(fp,@efi_file_info_id,finfosize,finfo);
    procsize:=finfo.filesize;
-   status:=fp^.efiRead(fp,procsize,proccontent);
+   proccontent:=efi_allocmem(procsize);
+   for j:=1 to procsize do 
+    begin
+     procindex:=1;
+     fp^.efiRead(fp,procindex,(proccontent+j-1)^);
+    end;
    if(status=efi_success) then break;
   end;
  if(i>count) then
   begin
-   efi_console_output_string(systemtable,'Boot failed,the kernel does not exist.'#10);
+   efi_console_output_string('Boot failed,the kernel does not exist.'#10);
    while(True) do;
   end;
  {Initialize the kernel parameter}
- loaderscreenconfig.screen_is_graphics:=fsi.header.tydqgraphics;
+ loaderscreenconfig.screen_is_graphics:=false;
  loaderscreenconfig.screen_address:=(gpl.graphics_item^)^.Mode^.FrameBufferBase;
  loaderscreenconfig.screen_width:=(gpl.graphics_item^)^.Mode^.Info^.HorizontalResolution;
  loaderscreenconfig.screen_height:=(gpl.graphics_item^)^.Mode^.Info^.VerticalResolution;
  {Free the system heap}
- freemem(gpl.graphics_item); gpl.graphics_count:=0;
- freemem(efsl.file_system_content); efsl.file_system_count:=0;
+ efi_freemem(gpl.graphics_item); gpl.graphics_count:=0;
+ efi_freemem(efsl.file_system_content); efsl.file_system_count:=0;
  {Check the elf kernel}
- bool[1]:=Pelf64_header(@proccontent)^.elf64_identify[1]=Byte(#$7F);
- bool[2]:=Pelf64_header(@proccontent)^.elf64_identify[2]=Byte('E');
- bool[3]:=Pelf64_header(@proccontent)^.elf64_identify[3]=Byte('L');
- bool[4]:=Pelf64_header(@proccontent)^.elf64_identify[4]=Byte('F');
+ bool[1]:=Pelf64_header(proccontent)^.elf64_identify[1]=Byte(#$7F);
+ bool[2]:=Pelf64_header(proccontent)^.elf64_identify[2]=Byte('E');
+ bool[3]:=Pelf64_header(proccontent)^.elf64_identify[3]=Byte('L');
+ bool[4]:=Pelf64_header(proccontent)^.elf64_identify[4]=Byte('F');
  if not(bool[1] and bool[2] and bool[3] and bool[4]) then
   begin
-   efi_console_output_string(systemtable,'Boot failed,the kernel is not the elf format file.'#10);
+   efi_console_output_string('Boot failed,the kernel is not the elf format file.'#10);
    while(True) do;
   end;
- bool[1]:=Pelf64_header(@proccontent)^.elf64_identify[5]=Byte(#$2);
+ bool[1]:=Pelf64_header(proccontent)^.elf64_identify[5]=Byte(#$2);
  if(bool[1]=false) then
   begin
-   efi_console_output_string(systemtable,'Boot failed,the kernel is not the elf 64-bit format file.'#10);
+   efi_console_output_string('Boot failed,the kernel is not the elf 64-bit format file.'#10);
    while(True) do;
   end;
  {Load the elf kernel}
- header:=Pelf64_header(@proccontent)^;
- program_headers:=@proccontent+header.elf64_program_header_offset;
+ header:=Pelf64_header(proccontent)^;
+ program_headers:=proccontent+header.elf64_program_header_offset;
  LowAddress:=$FFFFFFFFFFFFFFFF; HighAddress:=0;
  for i:=1 to header.elf64_program_header_number do
   begin
@@ -112,7 +123,7 @@ begin
       end;
     end;
   end;
- PageCount:=(HighAddress-LowAddress) shr 12+1;
+ PageCount:=(HighAddress-LowAddress) div 4096+2;
  SystemTable^.BootServices^.AllocatePages(AllocateAnyPages,efiLoaderCode,PageCount,KernelRelocateBase);
  RelocateOffset:=kernelRelocateBase-LowAddress;
  Zerostart:=Pointer(KernelRelocateBase);
@@ -124,29 +135,69 @@ begin
   begin
    if((program_headers+i-1)^.program_type=elf_program_table_load) then
     begin
-     SourceStart:=Pointer(@proccontent+(program_headers+i-1)^.program_offset);
+     SourceStart:=Pointer(proccontent+(program_headers+i-1)^.program_offset);
      DestStart:=Pointer((program_headers+i-1)^.program_virtual_address+RelocateOffset);
-     Move(DestStart^,SourceStart^,(program_headers+i-1)^.program_file_size);
+     Move(SourceStart^,DestStart^,(program_headers+i-1)^.program_file_size);
     end;
   end;
  KernelEntry:=Pointer(header.elf64_entry+RelocateOffset);
- efi_console_enable_mouse_blink(systemtable,false,0);
+ {For memory allocation only}
+ memorymap:=efi_loader_get_memory_map;
+ memoryavailable:=efi_loader_get_memory_available(memorymap);
+ if(memoryavailable.memory_size<1 shl 20+(1 shl 12)*sizeof(heap_segment)+(1 shl 28)*sizeof(graphics_item)+(1 shl 18)*sizeof(graphics_segment)+loaderscreenconfig.screen_width*loaderscreenconfig.screen_height*sizeof(graphics_color)+1 shl 22+(1 shl 16)*sizeof(heap_segment)) then
+  begin
+   efi_console_output_string('Error:memory size does not enough to start this operating system.'#10);
+   while True do;
+  end;
+ compheap_initialize(Pointer(memoryavailable.memory_address),1 shl 20,1 shl 12);
+ addressoffset:=1 shl 20+(1 shl 12)*sizeof(heap_segment);
+ graphics_heap_initialize(Pointer(memoryavailable.memory_address+addressoffset),1 shl 28,1 shl 18,loaderscreenconfig.screen_width,loaderscreenconfig.screen_height,Pointer(loaderscreenconfig.screen_address));
+ addressoffset:=addressoffset+(1 shl 28)*sizeof(graphics_item)+(1 shl 18)*sizeof(graphics_segment)+loaderscreenconfig.screen_width*loaderscreenconfig.screen_height*sizeof(graphics_color);
+ if(addressoffset+2 shl 30<memoryavailable.memory_size) then
+  begin
+   sysheap_initialize(Pointer(memoryavailable.memory_address+addressoffset),1 shl 30,1 shl 22);
+   addressoffset:=addressoffset+1 shl 30+(1 shl 22)*sizeof(heap_segment);
+  end
+ else if(addressoffset+1 shl 30<memoryavailable.memory_size) then
+  begin
+   sysheap_initialize(Pointer(memoryavailable.memory_address+addressoffset),1 shl 26,1 shl 19);
+   addressoffset:=addressoffset+1 shl 26+(1 shl 19)*sizeof(heap_segment);
+  end
+ else
+  begin
+   sysheap_initialize(Pointer(memoryavailable.memory_address+addressoffset),1 shl 22,1 shl 16);
+   addressoffset:=addressoffset+1 shl 22+(1 shl 16)*sizeof(heap_segment);
+  end;
+ efi_console_output_string('Let us enter the kernel.'#10);
  {Set the parameter for kernel}
- initparam:=allocmem(sizeof(sys_parameter_item));
- initparam^.item_content:=@loaderscreenconfig;
- initparam^.item_size:=sizeof(screen_config);
- param:=sys_parameter_construct(initparam,1);
+ initparam:=allocmem(sizeof(sys_parameter_item)*3);
+ initparam^.item_content:=@compheap;
+ initparam^.item_size:=sizeof(heap_record);
+ (initparam+1)^.item_content:=@graphheap;
+ (initparam+1)^.item_size:=sizeof(graphics_heap);
+ (initparam+2)^.item_content:=@sysheap;
+ (initparam+2)^.item_size:=sizeof(heap_record);
+ param:=sys_parameter_construct(initparam,3);
  {Execute the elf file}
  func.func:=sys_function(KernelEntry);
  funcandparam:=sys_parameter_and_function_construct(param,func,sizeof(return_config));
- partstr:=UintToWhex(Qword(@funcandparam.parameter_parameter));
- efi_console_output_string(systemtable,partstr);
- efi_console_output_string(systemtable,#10);
- Wstrfree(partstr);
+ efi_console_output_string('Entering the kernel......'#10);
  res:=sys_parameter_function_execute(funcandparam);
+ efi_console_output_hex(Preturn_config(res)^.m1);
+ efi_console_output_string(#10);
+ efi_console_output_number(Preturn_config(res)^.m2);
+ efi_console_output_string(#10);
+ efi_console_output_hex(Preturn_config(res)^.m3);
+ efi_console_output_string(#10);
+ efi_console_output_hex(Preturn_config(res)^.m4);
+ efi_console_output_string(#10);
+ efi_console_output_hex(Preturn_config(res)^.m5);
+ efi_console_output_string(#10);
+ freemem(res);
  {Free the memory}
  sys_parameter_and_function_free(funcandparam);
- SystemTable^.BootServices^.FreePages(KernelRelocateBase,PageCount);
+ freemem(initparam);
+ efi_console_output_string('Load kernel successfully!'#10);
  while True do;
  efi_main:=efi_success;
 end;
